7379e44164ebe96914f0d89d46fe16176a7dcddb
pourya bahramipour <pouryabahramipoor@gmail.com>
2023-05-17 23:18:00 +0330

    add header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header>
-      <div>
+    <header className="sticky top-0 p-5 flex justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        className="flex flex-row items-center"
+      >
         <SocialIcon
           url="https://linkedin.com/in/pourya-bahramipour-79a6ba93/"
           fgColor="gray"
@@ -22,6 +27,33 @@ export default function Header({}: Props) {
           fgColor="gray"
           bgColor="transparent"
         />
+        <SocialIcon
+          url="https://www.instagram.com/pourya_bahramipour"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://twitter.com/pouryataloo"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.facebook.com/pourya.papo.98/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+
+      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
       </div>
     </header>
   );
