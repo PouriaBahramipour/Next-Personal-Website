2e24d445cf877fd8e97d6644338ee27147b41f93
pourya bahramipour <pouryabahramipoor@gmail.com>
2023-05-25 00:43:02 +0330

    edit skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -15,10 +15,10 @@ export default function Skill({ srcImage, skillLevel }: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src={srcImage}
-        className="w-20 h-20 rounded-full object-cover  border-2 border-gray-500 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+        className="w-20 h-20 rounded-full object-cover  border-2 border-gray-500 md:w-24 md:h-24 xl:w-28 xl:h-28 filter group-hover:grayscale transition duration-300 ease-in-out"
       />
 
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-yellow-500 rounded-full w-20 h-20 md:w-28 md:h-28 xl:w-32 xl:h-32 ">
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-yellow-500 rounded-full w-20 h-20 md:w-24 md:h-24 xl:w-28 xl:h-28 ">
         <div className="flex items-center justify-center h-full">
           <p className="text-3xl font-bold opacity-100 text-black">
             {skillLevel}%
