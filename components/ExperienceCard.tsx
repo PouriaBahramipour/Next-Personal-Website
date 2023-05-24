425bc21e044410bbde6c4f6288d6f842289e862d
pourya bahramipour <pouryabahramipoor@gmail.com>
2023-05-24 23:17:30 +0330

    edit experience

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-type Props = {};
+type Props = {
+  experienceTitle: string;
+  companyName: string;
+  companyImage: string;
+  skills: {
+    skill: string;
+  }[];
+  startDate: string;
+};
 
-export default function ExperienceCard({}: Props) {
+export default function ExperienceCard({
+  experienceTitle,
+  companyName,
+  companyImage,
+  skills,
+  startDate,
+}: Props) {
   return (
     <article className="flex flex-col mt-8 snap-center bg-[#292929] p-4 rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] xl:mt-32 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden ">
       <motion.img
@@ -12,42 +26,23 @@ export default function ExperienceCard({}: Props) {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
-        src="https://media.licdn.com/dms/image/C4D12AQGp7-xvaiH1mA/article-cover_image-shrink_600_2000/0/1520122127770?e=2147483647&v=beta&t=SZesB47T-m-ShAUtRpgFI8fX2wQSo4meaxm-3bnoEQk"
+        src={companyImage}
       />
 
       <div className="px-0 md:px-10">
-        <h4 className="text-4xl font-light">Frontend Developer</h4>
-        <p className="font-bold text-2xl mt-1">Freelance</p>
+        <h4 className="text-4xl font-light">{experienceTitle}</h4>
+        <p className="font-bold text-2xl mt-1">{companyName}</p>
         <div className="flex space-x-2 my-2">
-          <img
-            className="h-10 w-10 rounded-full"
-            src="https://w7.pngwing.com/pngs/201/90/png-transparent-logo-html-html5.png"
-            alt=""
-          />
-          <img
-            className="h-10 w-10 rounded-full"
-            src="https://cdn.cdnlogo.com/logos/c/18/css.svg"
-            alt=""
-          />
-          <img
-            className="h-10 w-10 rounded-full"
-            src="https://w1.pngwing.com/pngs/136/126/png-transparent-javascript-logo-angularjs-nodejs-computer-programming-web-development-computer-software-jquery-yellow.png"
-            alt=""
-          />
-          <img
-            className="h-10 w-10 rounded-full"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png"
-            alt=""
-          />
-          <img
-            className="h-10 w-10 rounded-full"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Nuxt_logo.svg/1200px-Nuxt_logo.svg.png"
-            alt=""
-          />
+          {skills.map((item, index) => (
+            <img
+              key={index}
+              className="h-10 w-10 rounded-full"
+              src={item.skill}
+              alt=""
+            />
+          ))}
         </div>
-        <p className="uppercase py-5 text-gray-300">
-          Start Work Jan 2018 - Jun 2021 · 3 yrs 6 mos
-        </p>
+        <p className="uppercase py-5 text-gray-300">{startDate}</p>
 
         <ul className="list-none space-y-4 ml-5 text-lg">
           <li>summary point</li>
