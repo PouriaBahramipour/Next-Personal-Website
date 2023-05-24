0c5efc27310d78f2712cb2c2c1e6e6c14ce2aed4
pourya bahramipour <pouryabahramipoor@gmail.com>
2023-05-24 20:01:24 +0330

    add skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Skill from "./Skill";
 
 type Props = {};
 
@@ -8,6 +9,19 @@ export default function Skills({}: Props) {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Skills
       </h3>
+
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500">
+        Hover over a skill for currency profieciency
+      </h3>
+
+      <div className="grid grid-cols-4 gap-5">
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+      </div>
     </div>
   );
 }
