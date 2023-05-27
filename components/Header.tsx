87a2ef04fb4b9267985263fd8020ad5f38c8d550
pourya bahramipour <pouryabahramipoor@gmail.com>
2023-05-28 02:23:25 +0330

    final edit

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
 
-type Props = {};
-
-export default function Header({}: Props) {
+export default function Header() {
   return (
     <header className="sticky top-0 p-5 flex justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div
