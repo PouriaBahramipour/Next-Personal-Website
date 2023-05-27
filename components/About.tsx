457dcc7b6784135ecc606127318133a71bc267be
pourya bahramipour <pouryabahramipoor@gmail.com>
2023-05-28 02:23:50 +0330

    final edit

--- components/About.tsx
+++ components/About.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import aboutImage from "../src/assets/programmer.jpg";
 
-type Props = {};
-
-export default function About({}: Props) {
+export default function About() {
   return (
     <div className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
